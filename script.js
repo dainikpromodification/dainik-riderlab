8ba573be5f30d0c35a32b4efb91c165a102e8793
Tamim Iqbal <iqbaltamim1199@gmail.com>
2026-02-08 13:12:13 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function searchParts() {
+  const input = document.getElementById('partSearch').value.toLowerCase();
+  const list = document.getElementById('partsList').getElementsByTagName('li');
+
+  for (let i = 0; i < list.length; i++) {
+    let text = list[i].textContent || list[i].innerText;
+    if (text.toLowerCase().indexOf(input) > -1) {
+      list[i].classList.remove('hidden');
+    } else {
+      list[i].classList.add('hidden');
+    }
+  }
+}
